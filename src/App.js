cea62f2328b7d714141db6fe03e1c9c1775ed3bd
bridget462 <bridges.dev.create@gmail.com>
2020-09-06 21:04:08 +0900

    json place folder is used to generate users. Also, loading text added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CardList from "./CardList.js";
-import { robots } from "./robots.js";
 import SearchBox from "./SearchBox.js";
 import "./App.css";
 
@@ -8,9 +7,15 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -23,13 +28,17 @@ class App extends React.Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading ...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
