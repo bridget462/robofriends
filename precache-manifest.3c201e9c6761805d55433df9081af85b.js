9a69b87f17bc626760349a531c808b063c86c5f3
bridget462 <bridges.dev.create@gmail.com>
2020-09-07 10:38:19 +0900

    Updates

---
+++ precache-manifest.3c201e9c6761805d55433df9081af85b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9112e16c81333c3b0885bb440b37b73a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ca0b083208f2932f2fa0",
+    "url": "/robofriends/static/css/main.76eee9a4.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0b083208f2932f2fa0",
+    "url": "/robofriends/static/js/main.46fc371b.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
